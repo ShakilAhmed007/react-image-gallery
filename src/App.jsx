1a94979b93740af20bb7c512a80dbb61eb56f9a0
ShakilAhmed007 <sakilhossain01969.com>
2021-06-03 15:38:25 +0600

    tailwind set up done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          
+        </div>
+    </div>
+  );
+}
+
+export default App;
